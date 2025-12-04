ad1cd5b742850ac6a13328611d6c527d31d39e92
BASSAM ALHARBI. <101985240+BA-S12@users.noreply.github.com>
2025-12-04 11:49:06 +0300

    add message to josn file, add login logic and some refoctor

--- Client.js
+++ Client.js
@@ -1,47 +1,85 @@
-import WebSocket from "ws"
-import readline from "readline"
+import WebSocket from "ws";
+import readline from "readline";
+import fs from "fs";
 
+const socket = new WebSocket("ws://localhost:8080");
 
+const filePath = "./data/users.json";
 
-const socket = new WebSocket("ws://localhost:8080")
+const readMessages = () => {
+  try {
+    if (fs.existsSync(filePath)) {
+      const data = fs.readFileSync(filePath, "utf-8");
 
+      return JSON.parse(data || []);
+    }
+  } catch (err) {
+    console.log("Error reading file: " + err);
+  }
+  return [];
+};
 
+const startChat = (user) => {
+  console.log("Type 'exit' to quit");
 
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
-rl.setPrompt("--->")
-
-
-socket.onopen =()=>{
-    console.log("The server is connected")
-    console.log("Type 'exit' to quit")
-    rl.prompt()
-
-}
-
-
-socket.onmessage = (event)=>{
-    console.log(event.data.toString())
-    rl.prompt()
-}
-
-
-rl.on("line", (input)=>{
-    if(input.trim().toLowerCase() =="exit"){
-        socket.close()
-        rl.close()
-        return;
+  rl.setPrompt(`${user.name} >`);
+  rl.prompt();
+  rl.on("line", (input) => {
+    if (input.trim().toLowerCase() == "exit") {
+      socket.close();
+      rl.close();
+      return;
     }
 
-        socket.send(input)
-        rl.prompt()
+    socket.send(input);
+    rl.prompt();
+  });
+};
 
-    
-})
-socket.onclose = ()=>{
-    console.log("Disconnected")
-}
+const handleLogin = () => {
+  rl.question("Enter yout ID: (if you haven`t account enter 0)", (id) => {
+    const data = fs.readFileSync(filePath, "utf-8");
+    const dataArray = JSON.parse(data);
+    const foundUser = dataArray.find((user) => user.id === Number(id));
 
+    if (!foundUser || id === 0) {
+      const ids = dataArray.map((user) => user.id);
+      const maxId = Math.max(...ids);
+
+      rl.question("Enter youe name: ", (name) => {
+        const user = {
+          id: maxId + 1,
+          name,
+        };
+        const cuurentUsers = readMessages();
+        cuurentUsers.push(user);
+        fs.writeFileSync(filePath, JSON.stringify(cuurentUsers));
+      });
+    }
+
+    if (foundUser) {
+      console.log(`Welcome ${foundUser.name}`);
+      startChat(foundUser);
+    }
+  });
+};
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.setPrompt("--->");
+
+socket.onopen = () => {
+  console.log("The server is connected");
+    handleLogin();
+};
+
+socket.onmessage = (event) => {
+  console.log(event.data.toString());
+  rl.prompt();
+};
+
+socket.onclose = () => {
+  console.log("Disconnected");
+};
--- server.js
+++ server.js
@@ -1,17 +1,42 @@
-import {WebSocketServer} from "ws"
-import fs from "fs"
+import { WebSocketServer } from "ws";
+import fs from "fs";
 
-const PORT = 8080
-const filePath = "./message.txt"
+const PORT = 8080;
+const filePath = "./data/message.json";
 
-const wss = new WebSocketServer({port:PORT})
+const wss = new WebSocketServer({ port: PORT });
+
+
+const readMessages = ()=>{
+    try{
+        if(fs.existsSync(filePath)){
+            const data = fs.readFileSync(filePath,"utf-8")
+            return JSON.parse(data || [])
+        }
+    }catch(err){
+        console.log("Error reading file: "+err)
+    }
+    return [];
+}
+
+
 
 console.log("WebSocket Server running on port", PORT);
-wss.on("connection", (ws)=>{
-    console.log("Client Connected")
+wss.on("connection", (ws) => {
+  console.log("Client Connected");
 
-    ws.on("message", (msg)=>{
-        console.log("msg: "+msg)
-        fs.appendFileSync(filePath, msg)
-    })
-})
+  ws.on("message", (msg) => {
+    console.log("msg: " + msg);
+    const message = {
+      id: Date.now().toString(),
+      timestamb:new Date().toISOString(),
+      message: msg.toString(),
+    };
+
+    const currentMessages = readMessages();
+    currentMessages.push(message);
+    
+
+    fs.writeFileSync(filePath, JSON.stringify(currentMessages));
+  });
+});
